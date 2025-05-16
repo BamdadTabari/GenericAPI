fd402a652dba5300cb06d59a557d50135fa9fb86
Uncle Bamdad <mohammadJavadTabari1024@outlook.com>
2025-05-16 04:21:58 -0700

    Base Folder

---
+++ DataLayer/Base/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer;
+public interface IBaseEntity { }
+
+public class BaseEntity : IBaseEntity
+{
+    public long id { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+    public bool is_deleted { get; set; }
+
+    [MaxLength(1000)]
+    public string slug { get; set; }
+}
